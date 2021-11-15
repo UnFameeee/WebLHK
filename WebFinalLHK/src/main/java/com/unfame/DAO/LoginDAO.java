ad19b23505c6c4860b1b18d33c105b0b509c0cc4
Ng. Hoang Vu <vu2872001@gmail.com>
2021-11-15 16:44:52 +0700

    jdbc login và register
    sửa lại DAO của register và thêm controller và DAO của login

---
+++ WebFinalLHK/src/main/java/com/unfame/DAO/LoginDAO.java
@@ -0,0 +1,54 @@
+package com.unfame.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.unfame.Model.Account;
+
+public class LoginDAO {
+	 String driver = "com.mysql.jdbc.Driver";
+	 String connectionUrl = "jdbc:mysql://localhost:3306/";
+	 String database = "WebLHK?useSSL=false";
+	 String userid = "root";
+	 String password = "root";
+	 
+	 private static final String CHECK_ACC = "select * from Member where Email = ? and Password = ?";
+	 
+	 protected Connection getConnection(){
+		 Connection connection = null;
+	     try{
+	    	 Class.forName(driver);
+	         connection = DriverManager.getConnection(connectionUrl + database, userid, password);
+
+	     } 
+	     catch (ClassNotFoundException | SQLException e){
+	         e.printStackTrace();
+	     }
+	     
+	     return connection;
+	 }
+	 
+	 public boolean checkAccount (Account acc){
+		 boolean check = false;
+		 
+		 try {
+			Connection con = getConnection();
+				
+			PreparedStatement ppstm = con.prepareStatement(CHECK_ACC);			
+			ppstm.setString(1, acc.getEmail());
+			ppstm.setString(2, acc.getPassword());
+				
+			ResultSet rs = ppstm.executeQuery();
+			check = rs.next();
+				
+		 }
+		 catch (Exception e){
+			 e.printStackTrace();
+		 }
+		 
+		 return check;
+	 }
+}
