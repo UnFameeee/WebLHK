6f8bacbf176f6d7f94eafbe8275b0ad4c10d762b
Quốc Thắng <quocthang036@gmail.com>
2021-11-27 01:43:25 +0700

    Fix bug view
    Fix bug view

--- WebFinalLHK/src/main/java/com/unfame/global/IdGlobal.java
+++ WebFinalLHK/src/main/java/com/unfame/global/IdGlobal.java
@@ -2,7 +2,7 @@ package com.unfame.global;
 
 public class IdGlobal {
     public static int UserId = -1;
-    public static String Role = "Member";
+    public static String Role = "Admin";
     public static int PageLIMIT = 0;
     public static boolean maxPage = false;
     public static boolean minPage = false;
